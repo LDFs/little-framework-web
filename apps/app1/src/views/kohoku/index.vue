dc71305bc62575e1ad9bb5806c0c7d773071fa51
lindongfeng <lindf@das.com>
2025-07-21 17:24:14 +0800

    --n 捣鼓

--- apps/app1/src/views/kohoku/index.vue
+++ apps/app1/src/views/kohoku/index.vue
@@ -22,20 +22,31 @@ function tryRender() {
     return;
   }
   // 顶点着色器
+  // vec4的4个参数分别为x,y,z,w齐次坐标（x/w, y/w, z/w）
   const VERTEX_SHADER_SOURCE = `
+    attribute vec4 a_position;
     void main() {
-      gl_Position = vec4(0.1, 0.1, 0.1, 1.0);
-      gl_PointSize = 30.0;
+      // 矩形
+      // gl_Position = vec4(0.1, 0.1, 0.1, 1.0);
+      // gl_PointSize = 30.0;
+      // 三角形
+      gl_Position = a_position;
     }
   `;
   // 片元着色器
+  // vec4的4个参数分别为r,g,b,a，颜色
   const FRAGMENT_SHADER_SOURCE = `
     void main() {
+      // gl_FragColor = vec4(1, 0, 0.5, 1);
       gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
     }
   `;
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, VERTEX_SHADER_SOURCE);
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, FRAGMENT_SHADER_SOURCE);
+  const fragmentShader = createShader(
+    gl,
+    gl.FRAGMENT_SHADER,
+    FRAGMENT_SHADER_SOURCE
+  );
 
   const program = createProgram(gl, vertexShader, fragmentShader);
   draw(gl, program);
@@ -58,7 +69,11 @@ function createShader(gl: WebGLRenderingContext, type: GLenum, source: string) {
   return shader;
 }
 
-function createProgram(gl: WebGLRenderingContext, vertexShader: WebGLShader, fragmentShader: WebGLShader) {
+function createProgram(
+  gl: WebGLRenderingContext,
+  vertexShader: WebGLShader,
+  fragmentShader: WebGLShader
+) {
   // 创建一个程序对象
   const program = gl.createProgram();
   // 将着色器附加到程序上
@@ -78,7 +93,10 @@ function draw(gl: WebGLRenderingContext, program: WebGLProgram) {
   const positionBuffer = gl.createBuffer();
   // 将缓冲区对象绑定到目标
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+  // 矩形
+  // const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+  // 三角形
+  const positions = [0, 0.1, 0, 0.5, 1, 0];
   // 创建并初始化缓冲区对象的数据存储
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
@@ -89,7 +107,7 @@ function draw(gl: WebGLRenderingContext, program: WebGLProgram) {
   const offset = 0;
   // 指定顶点数据布局
   gl.vertexAttribPointer(
-    gl.getAttribLocation(program, "aVertexPosition"),
+    gl.getAttribLocation(program, "a_position"), // a_position 对应顶点着色器的东西
     numComponents,
     type,
     normalize,
@@ -97,16 +115,17 @@ function draw(gl: WebGLRenderingContext, program: WebGLProgram) {
     offset
   );
   // 启用一个顶点属性数组
-  gl.enableVertexAttribArray(gl.getAttribLocation(program, "aVertexPosition"));
+  gl.enableVertexAttribArray(gl.getAttribLocation(program, "a_position"));
 
   // 设置视口
-  gl.viewport(0, 0, gl.canvas.width/2, gl.canvas.height/2);
+  gl.viewport(0, 0, gl.canvas.width / 2, gl.canvas.height / 2);
   // 指定清空颜色缓冲区时使用的颜色
   gl.clearColor(0.0, 0.0, 0.0, 0.1);
   // 清空缓冲区到预设值
   gl.clear(gl.COLOR_BUFFER_BIT);
   // 从数组数据渲染基元
-  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 3);
+  gl.drawArrays(gl.TRIANGLES, 0, 3); // 三角形
+  // gl.drawArrays(gl.POINTS, 0, 1);   // 矩形
 }
 </script>
 
